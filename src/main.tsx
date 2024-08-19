9e8226aa39af572cfadc85b28453170d10944704
GOB8CA <gob8ca@bosch.com>
2024-08-19 08:00:37 -0300

    :truck: feat: creating routes for system

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+import RegisterItem from "./Pages/RegisterAItem/RegisterItem";
+import UseCase from "./Pages/UseCase/UseCase";
+import Footer from "./components/Footer/Footer";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/register_a_item" element={<RegisterItem/>}/>
+          <Route path="/use_case" element={<UseCase/>}/>
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
+  </React.StrictMode>
 )
