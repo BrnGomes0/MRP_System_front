3381d809190635ffa513b32ab895786894eea0ee
GOB8CA <gob8ca@bosch.com>
2024-08-19 08:01:33 -0300

    :wrench: fix: configure the settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppinsFont': ["Poppins", "sans-serif"]
+      },
+      fontWeight: {
+        'mediumBold': 510
+      }
+    },
   },
   plugins: [],
 }
