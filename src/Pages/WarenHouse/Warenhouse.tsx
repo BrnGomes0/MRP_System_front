b1773af3da6982591617dfd5669093cf7dfc4644
BrnGomes0 <brunownog05@gmail.com>
2024-08-20 16:22:01 -0300

    :sparkles: feat: creating a table for warenhouse

---
+++ src/Pages/WarenHouse/Warenhouse.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WarenhouseTable from "../../components/WarenhouseTable/WarenhouseTable";
+
+
+const Warenhouse: React.FC = () => {
+    return(
+        <section className="pt-[73px]">
+            <WarenhouseTable/>
+        </section>
+    )
+}
+
+export default Warenhouse
---
+++ src/components/WarenhouseTable/WarenhouseTable.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const WarenhouseTable: React.FC = () => {
+    return(
+        <div className="">
+            <table>
+                <thead>
+                    <tr>
+                        <th>nome</th>
+                        <th>nome</th>
+                        <th>nome</th>
+                        <th>nome</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    )
+}
+
+export default WarenhouseTable
