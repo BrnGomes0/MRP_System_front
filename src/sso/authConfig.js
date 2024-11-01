64d5405986c88bfba8bb1b394178212751cc666e
Bruno Gomes <132568509+BrnGomes0@users.noreply.github.com>
2024-11-01 09:21:42 -0300

    Update authConfig.js

--- src/sso/authConfig.js
+++ src/sso/authConfig.js
@@ -1,8 +1,8 @@
 export const msalConfig = {
     auth: {
-        clientId: `60fb1405-2498-4f01-93e3-97022fee0a42`,
-        authority: `https://login.microsoftonline.com/0ae51e19-07c8-4e4b-bb6d-648ee58410f4`,
-        redirectUri: `http://localhost:5173/use-case`,
+        clientId: ``,
+        authority: ``,
+        redirectUri: ``,
     },
     cache: {
       cacheLocation: "sessionStorage", // This configures where your cache will be stored
