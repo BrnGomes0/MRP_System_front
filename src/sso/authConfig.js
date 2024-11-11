c7dd2c3055bbcdcf74007b1ca2ef077f7608a3f0
Bruno Gomes <132568509+BrnGomes0@users.noreply.github.com>
2024-11-11 11:39:55 -0300

    Update authConfig.js

--- src/sso/authConfig.js
+++ src/sso/authConfig.js
@@ -3,8 +3,8 @@ import process from "process";
 export const msalConfig = {
   
     auth: {
-        clientId: "60fb1405-2498-4f01-93e3-97022fee0a42",
-        authority: "https://login.microsoftonline.com/0ae51e19-07c8-4e4b-bb6d-648ee58410f4",
+        clientId: "",
+        authority: "",
         redirectUri: "/use_case",
     },
     cache: {
